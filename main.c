2814610a2ab8874062776801601ae637ac3925c2
khardani <majdikhardani1998@gmail.com>
2022-10-27 22:07:47 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,63 @@
+
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Khardani Majdi
+  * @brief   How to develop structured code 
+  * @version V1.0.0
+  * @date    28-10-2022
+  *****************************************************************************/
+/* Includes ------------------------------------------------------------------*/
+
+/* Macros --------------------------------------------------------------------*/
+//*define som
+#define som(x,y) x+y
+//#define Mmult
+#define mult(x,y) x*y
+//#define max
+#define max(x,y) x > y ? x : y
+//#define min
+#define min(x,y) x < y ? x: y
+//#define OTHERS
+
+
+/* Private function prototypes -----------------------------------------------*/
+int SOM (int a,int b){
+  return(a+b);
+}
+
+int MULT(int a,int b){
+  return(a*b);
+}
+
+int MAX(int a,int b){
+  return(a > b ? a : b);
+}
+
+int MIN(int a,int b){
+  return(a < b ? a: b);
+}
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+
+int main(void)
+{
+
+
+
+  while (1)
+  {
+   
+   
+  }
+}
+
+
+
+/**************************************END OF FILE**************************************/
